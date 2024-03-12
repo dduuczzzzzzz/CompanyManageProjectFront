2831ecaca97f83c6e711074207891591eca51d52
Duc <duc8anv2@gmail.com>
2024-03-12 22:54:32 +0700

    Add lob and init model page

--- src/pages/checkIn/index.tsx
+++ src/pages/checkIn/index.tsx
@@ -1,6 +1,12 @@
+import Model from '../../components/checkIn/model'
 import MainLayout from '../../components/layouts/main'
 
 const CheckInPage = () => {
-  return <MainLayout>CheckIN pages</MainLayout>
+  return (
+    <MainLayout>
+      <p>CheckIN pages</p>
+      <Model />
+    </MainLayout>
+  )
 }
 export default CheckInPage
